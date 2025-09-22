03ec1e9a14dafdc3f79dc576df49a484e9c8cac9
supremehato <charliemurtagh1717@gmail.com>
2025-09-22 11:56:21 +0100

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -25,11 +25,11 @@ local osdate = os.date
 
 --// Launching
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Roblox-Functions-Library/main/Library.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/Library.lua"))()
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/UI%20Library.lua"))()
-local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/ESP.lua"))()
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/UI%20Library.lua"))()
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/ESP.lua"))()
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/Aimbot.lua"))()
 
 --// Variables
 
