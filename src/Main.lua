edc97a8ec4532bb25d2a90d78ba1371383414ff6
supremehato <charliemurtagh1717@gmail.com>
2025-09-22 10:46:15 +0100

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -29,7 +29,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHu
 
 local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/refs/heads/main/src/UI%20Library.lua"))()
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/refs/heads/main/src/ESP.lua"))()
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/refs/heads/main/src/Aimbot.lua"))()
 
 --// Variables
 
