be360feb4afb5af59cdf5a86c01b313ee07caf03
supremehato <charliemurtagh1717@gmail.com>
2025-09-22 11:30:08 +0100

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -1,7 +1,7 @@
 --[[
 
-	AirHub V2 by Exunys © CC0 1.0 Universal (2023)
-	https://github.com/Exunys
+	AirHub V2 by Supreme © CC0 1.0 Universal (2023)
+	https://github.com/Supreme
 
 ]]
 
@@ -25,7 +25,7 @@ local osdate = os.date
 
 --// Launching
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Roblox-Functions-Library/main/Library.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Supreme/Roblox-Functions-Library/main/Library.lua"))()
 
 local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/UI%20Library.lua"))()
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/supremehato/SupremeHub/main/src/ESP.lua"))()
@@ -932,12 +932,12 @@ ProfilesSection:Button({
 	end
 })
 
-InformationSection:Label("Made by Exunys")
+InformationSection:Label("Made by Supreme")
 
 InformationSection:Button({
 	Name = "Copy GitHub",
 	Callback = function()
-		setclipboard("https://github.com/Exunys")
+		setclipboard("https://github.com/Supreme")
 	end
 })
 
